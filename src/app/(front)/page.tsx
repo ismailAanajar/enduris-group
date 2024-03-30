c28fb1fdeaceacc2c0a05a7e96e9aa936d7bf7e9
Ismail Anajar <omaranajar@gmail.com>
2024-03-30 15:37:47 +0000

    pull the img

--- src/app/(front)/page.tsx
+++ src/app/(front)/page.tsx
@@ -1,10 +1,15 @@
+import prisma from "@/db";
 import Image from "next/image";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
 
 export default async function Home() {
+  const notif = await prisma.notification.findFirst({
+    where: {
+      id: 1,
+    },
+  });
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Image src={notif?.image || ""} alt="hello" width={200} height={200} />
+    </main>
   );
 }
