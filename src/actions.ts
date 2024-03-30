2c6392c076086151488cf15ffc72a6309515b10f
Ismail Anajar <omaranajar@gmail.com>
2024-03-30 13:57:21 +0000

    add validation

--- src/actions.ts
+++ src/actions.ts
@@ -1,9 +1,18 @@
 "use server";
 import fs from "fs/promises";
+import { z } from "zod";
+
+const schema = z.object({
+  file: z.instanceof(File),
+});
 
 export async function upload(formData: FormData) {
   console.log(Object.fromEntries(formData.entries()));
-  const data = Object.fromEntries(formData.entries());
+  const result = schema.safeParse(Object.fromEntries(formData.entries()));
+  if (!result.success) {
+    return "error";
+  }
+  const data = result.data;
   await fs.mkdir("public/products", { recursive: true });
   const path = `/products/${crypto.randomUUID()}-${data.file?.name}`;
   await fs.writeFile(
