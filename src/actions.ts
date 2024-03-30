8c775fa309216af9b0e054b703d8c4a312d341e2
Ismail Anajar <omaranajar@gmail.com>
2024-03-30 15:22:30 +0000

    create notif

--- src/actions.ts
+++ src/actions.ts
@@ -1,6 +1,7 @@
 "use server";
 import fs from "fs/promises";
 import { z } from "zod";
+import prisma from "./db";
 
 const schema = z.object({
   file: z.instanceof(File),
@@ -19,4 +20,10 @@ export async function upload(formData: FormData) {
     `public${path}`,
     Buffer.from(await data.file.arrayBuffer())
   );
+  await prisma.notification.create({
+    data: {
+      title: "hello notif",
+      image: path,
+    },
+  });
 }
